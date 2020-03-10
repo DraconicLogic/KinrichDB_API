2f3851276abc1ed0d3a10796982d9f19468135ee
Kingsley Onyensoh <onyensoh1987@hotmail.com>
2020-03-10 22:41:32 +0000

    separate firebase credentials to format for heroku

--- db/firebase.js
+++ db/firebase.js
@@ -1,7 +1,18 @@
 const admin = require('firebase-admin')
-const { DB_URL, CREDS } = process.env
-let newCreds = CREDS
-if (!CREDS) {
+const { 
+  DB_URL,
+  PRIVATE_KEY,
+  PROJECT_ID,
+  CLIENT_EMAIL,
+  NODE_ENV
+} = process.env
+
+let newCreds = {
+  projectId: PROJECT_ID,
+  privateKey: PRIVATE_KEY ? PRIVATE_KEY.replace(/\\n/g, '\n'): null,
+  clientEmail: CLIENT_EMAIL
+}
+if (NODE_ENV !== 'production') {
   newCreds = require('./nnenna-textiles-firebase-adminsdk-e6e1y-6201c988e2.json')
 }
 
