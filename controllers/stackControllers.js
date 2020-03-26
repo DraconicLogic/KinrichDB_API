e27fc71a84ba5ea176fee24562dffbd9db8e6dcb
Kingsley Onyensoh <onyensoh1987@hotmail.com>
2020-03-26 21:18:04 +0000

    change removeStacksById function to an async function

--- controllers/stackControllers.js
+++ controllers/stackControllers.js
@@ -1,4 +1,3 @@
-
 const StackModel = require('../models/stackModels')
 
 
@@ -12,11 +11,16 @@ async function getStacks(req, res, next){
   res.status(200).send({stacks})
 }
 
-function removeStacksById(req, res, next){
+async function removeStacksById(req, res, next){
     console.log('REMOVING STACKS')
     const {usedCodes} = req.body
+    console.log(req.body)
     console.log(usedCodes)
-   
+  
+  const removedStack = await StackModel.deleteOne({stackId:usedCodes[0]})
+
+  console.log(removedStack)
+  res.status(200).send({removedStack})
 }
 
 module.exports = {addStack, getStacks, removeStacksById}
