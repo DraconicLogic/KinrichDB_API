41e127b2ed457170870ed7001a410f4b6ccf385e
Kingsley Onyensoh <onyensoh1987@hotmail.com>
2020-04-05 11:50:33 +0100

    Change order of containerSchema

--- models/containerModels.js
+++ models/containerModels.js
@@ -2,14 +2,13 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const containerSchema = new Schema({
+  containerNumber: 'string',
   sealNumber: 'string',
+  date: 'string',
   containerContent: [{
     stackContent: ['string'],
     stackId: 'string' 
-  }],
-  containerNumber: 'string',
-  date: 'string'
-
+  }]
 })
 
 module.exports = ContainerModel = mongoose.model('Container', containerSchema)
