8f1647e99a1c197446d4323b274b918a4d291d23
Kingsley Onyensoh <onyensoh1987@hotmail.com>
2021-09-14 05:47:06 +0100

    Updates CORS options to prevent cors policy issues

--- utils/corsConfig.js
+++ utils/corsConfig.js
@@ -1,8 +1,8 @@
 const corsOptions = {
-  origin: "*",
+  origin: true,
   optionsSuccessStatus: 204,
   methods: ["GET", "POST", "DELETE", "OPTIONS", "PUT", "PATCH"],
-  allowedHeader: "Content-Type",
+  allowedHeader: ["Content-Type", "application/json"]
 }
 
 module.exports = {corsOptions}
