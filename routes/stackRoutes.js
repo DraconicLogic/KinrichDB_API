0f5c5006978265c0c39948170c1da1c8328a1831
Kingsley Onyensoh <onyensoh1987@hotmail.com>
2022-02-16 06:40:50 +0000

    pass first test for overwrite stacks implementation

--- routes/stackRoutes.js
+++ routes/stackRoutes.js
@@ -1,10 +1,11 @@
 const stackRouter = require('express').Router()
-const {createStack, getStacks, removeStacksById} = require('../controllers/stackControllers.js')
+const {createStack, getStacks, removeStacksById, overwriteStacks} = require('../controllers/stackControllers.js')
 const cors = require('cors')
 
 stackRouter.route('/')
   .get(getStacks)
   .post(cors(), createStack)
   .delete(removeStacksById)
+  .put(overwriteStacks)
 
 module.exports = stackRouter
